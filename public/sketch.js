f1bd45ae8d32d57236c5574a48825ce186f5fcb0
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-06-20 05:25:26 +0200

    Handling post, cors and add afinn full list

--- public/sketch.js
+++ public/sketch.js
@@ -1,29 +1,24 @@
 function setup () {
   createCanvas(windowWidth, 400)
   drawData()
+  handleOnSubmit()
+  handleOnAnalize()
+}
 
-  const button = select('#submit')
-  button.mousePressed(() => {
+function handleOnSubmit () {
+  const submitButton = select('#submit')
+
+  submitButton.mousePressed(() => {
     const word = select('#word')
     const score = select('#score')
 
-    word.mousePressed(() => {
-      word.removeClass('error')
-    })
-    score.mousePressed(() => {
-      score.removeClass('error')
-    })
-
-    if (!word.value().length) {
-      word.addClass('error')
-    }
-    if (!score.value().length) {
-      score.addClass('error')
-    }
+    handleInteraction(word)
+    handleInteraction(score)
 
     if (word.value().length && score.value().length) {
       word.removeClass('error')
       score.removeClass('error')
+
       loadJSON('/add/' + word.value() + '/' + score.value(), (data) => {
         word.value('')
         score.value('')
@@ -33,25 +28,51 @@ function setup () {
   })
 }
 
-function drawData () {
-  loadJSON('/all', gotData)
+function handleOnAnalize () {
+  const analyzeButton = select('#analyze')
+  analyzeButton.mousePressed(() => {
+    const txt = select('#textareainput')
+    handleInteraction(txt)
+
+    if (txt.value().length) {
+      const data = { text: txt.value() }
+      httpPost('/analyze', data, 'json', dataPosted, postError)
+    }
+  })
 }
 
-function gotData (data) {
-  background(51)
-  const keys = Object.keys(data)
-  const threshold = 48
+function dataPosted (result) {
+  console.log(result)
+}
 
-  for (let i = 0; i < keys.length; i++) {
-    let word = keys[i]
-    let score = data[word]
-    let x = random(threshold, width - threshold)
-    let y = random(threshold, height - threshold)
-    fill(255)
-    textFont('monospace')
-    textSize(score * 7)
-    text(word, x, y)
+function postError (error) {
+  console.log(error)
+}
+
+function handleInteraction (ele) {
+  ele.mousePressed(() => {
+    ele.removeClass('error')
+  })
+  if (!ele.value().length) {
+    ele.addClass('error')
   }
 }
 
-// function draw () {}
+function drawData () {
+  loadJSON('/all', data => {
+    background(51)
+    const keys = Object.keys(data)
+    const threshold = 48
+
+    for (let i = 0; i < keys.length; i++) {
+      let word = keys[i]
+      let score = data[word]
+      let x = random(threshold, width - threshold)
+      let y = random(threshold, height - threshold)
+      fill(255)
+      textFont('monospace')
+      textSize(score * 7)
+      text(word, x, y)
+    }
+  })
+}
